620780e26723bed6d0e8583403220f3f4b1f0eca
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-09-20 01:23:58 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import ABI from './ABI.json';
+import Logo from './Logo.png';
+
+const contractAddress = "0xD748F1C8C250f1bF1B9EFadd156aBD6EaE5F5143";
+
+function App() {
+
+  const [connected, setConnected] = useState(false);
+  const [minted, setMinted] = useState(false);
+  const [name, setName] = useState(null)
+
+  const connect = async () => {
+    try {
+      let provider;
+      provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const network = await provider.getNetwork();
+      const desiredChainId = '0xAA36A7';
+      if (network.chainId !== parseInt(desiredChainId)) {
+        try {
+          await window.ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: desiredChainId }],
+          });
+        } catch (switchError) {
+          if (switchError.code === 4902) {
+            try {
+              await window.ethereum.request({
+                method: 'wallet_addEthereumChain',
+                params: [{
+                  chainId: desiredChainId,
+                  chainName: 'Sepolia',
+                  nativeCurrency: {
+                    name: 'ETH',
+                    symbol: 'ETH',
+                    decimals: 18
+                  },
+                  rpcUrls: ['https://rpc.sepolia.org'],
+                  blockExplorerUrls: ['https://sepolia.etherscan.io'],
+                }],
+              });
+            } catch (addError) {
+              throw addError;
+            }
+          } else {
+            throw switchError;
+          }
+        }
+      }
+      provider = new ethers.providers.Web3Provider(window.ethereum)
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const _userAddress = await signer.getAddress();
+      const { ethereum } = window;
+      if (ethereum) {
+        const ensProvider = new ethers.providers.InfuraProvider('mainnet');
+        const displayAddress = _userAddress?.substr(0, 6) + "...";
+        const ens = await ensProvider.lookupAddress(_userAddress);
+        if (ens !== null) {
+          setName(ens)
+
+        } else {
+          setName(displayAddress)
+        }
+      }
+
+      await signer.signMessage("Welcome to Web3 Builders!");
+      setConnected(true)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  const mint = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const address = signer.getAddress();
+      const contract = new ethers.Contract(contractAddress, ABI, signer);
+      const tx = await contract.safeMint(address);
+      await tx.wait()
+      setMinted(true)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  const disconnect = async () => {
+    setConnected(false)
+    setMinted(false)
+  }
+  
+  return (
+    <div className="app">
+      {!connected && <button className='connect-btn' onClick={connect}>connect</button>}
+      {connected && <button className='disconnect-btn' onClick={disconnect}>{name}</button>}
+
+      {connected && (
+        <div className='card'>
+          {!minted && (
+          <>
+            <img src={Logo} alt='logo' />
+            <hr />
+            <button onClick={mint}>MINT</button>
+          </>
+          )}
+          {minted && (
+          <>
+            <img src={Logo} alt='logo' />
+            <p>Minted! Thank you 🙏</p>
+          </>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
